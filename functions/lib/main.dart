568118006c317b56a9008d5eb9761d49cd6e10bf
dev-Roshan-lab <dev.vividgoat@gmail.com>
2024-01-15 17:19:52 +0530

    fix:JsonCodec-decode

--- functions/lib/main.dart
+++ functions/lib/main.dart
@@ -6,19 +6,19 @@ Future<dynamic> main(final context) async {
   String year = context.req.query['year'];
   String department = context.req.query['department'];
   String section = context.req.query['section'];
-
   String timetable = "${year}_${department}_$section.csv";
+  context.log(timetable);
 
-  Map jsonData = jsonDecode(await csvToJson("timetables/$timetable"));
+  final jsonData = await csvToJson(context, "timetables/$timetable");
   print(jsonData);
   return context.res.json(json);
 }
 
-Future<String> csvToJson(String filePath) async {
+Future<String> csvToJson(final context, String filePath) async {
   try {
     final file = File(filePath);
     final csvData = await file.readAsString();
-    final parsedCsv = await CsvToListConverter().convert(csvData);
+    final parsedCsv = CsvToListConverter().convert(csvData);
 
     // Create the desired JSON structure
     final jsonData = <String, List>{};
@@ -30,7 +30,7 @@ Future<String> csvToJson(String filePath) async {
 
     return jsonEncode(jsonData);
   } catch (error) {
-    print('Error reading CSV file: $error');
+    context.error('Error reading CSV file: $error');
     return '{"error": "Failed to read CSV file"}';
   }
 }
