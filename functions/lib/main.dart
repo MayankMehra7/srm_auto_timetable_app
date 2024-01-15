ab159febe48941dfd8c1738bdd060fdcbd33fca0
dev-Roshan-lab <dev.vividgoat@gmail.com>
2024-01-15 16:57:31 +0530

    chore:added-timetables

--- functions/lib/main.dart
+++ functions/lib/main.dart
@@ -1,8 +1,36 @@
-Future<dynamic> main(final context) async {
-  String department = context.req.query['dept'];
-  String section = context.req.query['sec'];
+import 'dart:convert';
+import 'dart:io';
+import 'package:csv/csv.dart';
 
-  // TODO: implement JSON query of the TT
-  List todayClass = [];
-  return context.res.json({'periods': todayClass});
+Future<dynamic> main(final context) async {
+  String year = context.req.query['year'];
+  String department = context.req.query['department'];
+  String section = context.req.query['section'];
+
+  String timetable = "${year}_${department}_$section.csv";
+
+  Map jsonData = jsonDecode(await csvToJson("timetables/$timetable"));
+  print(jsonData);
+  return context.res.json(json);
+}
+
+Future<String> csvToJson(String filePath) async {
+  try {
+    final file = File(filePath);
+    final csvData = await file.readAsString();
+    final parsedCsv = await CsvToListConverter().convert(csvData);
+
+    // Create the desired JSON structure
+    final jsonData = <String, List>{};
+    for (final row in parsedCsv) {
+      final day = row[0];
+      final values = row.sublist(1); // Skip the first column (day)
+      jsonData[day] = values;
+    }
+
+    return jsonEncode(jsonData);
+  } catch (error) {
+    print('Error reading CSV file: $error');
+    return '{"error": "Failed to read CSV file"}';
+  }
 }
--- functions/lib/timetable.dart
+++
@@ -1,4 +0,0 @@
-Map dayOrder = {};
-Map timeTable = {
-  "BCA_A": {"D1": [], "D2": [], "D3" : [], "D4": [], "D5": []}
-};
