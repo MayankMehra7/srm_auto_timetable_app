104bc1dbef5c8215ce06ce6b522517f1fc9a01f5
dev-Roshan-lab <dev.vividgoat@gmail.com>
2024-01-20 14:55:13 +0530

    fix:update file name

--- python/day_order.py
+++ python/day_order.py
@@ -1,7 +1,9 @@
 def main(context):
-    if ('date' in context.req.query):
+    if (context.req.path == "/"):
+        return context.res.json({"msg": "working, use /subscribe to register users"})
+    if (context.req.path == "/dayorder"):
         return context.res.json({"msg" : day_order_data[context.req.query['date']]["do"]})
-    return context.res.json({"msg": "no date provided"})
+    return context.res.json({"msg": "no date provided or invalid path"})
 
 day_order_data = {
     "01-01-24": {
