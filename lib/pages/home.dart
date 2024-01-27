7048db1d0b9a3296bf479efa52ea0d9d18880beb
pxnchxm1 <panchamipkumar1995@gmail.com>
2024-01-27 17:05:50 +0530

    feat:added_dialogueBox_In_HomePage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,9 +2,6 @@
 
 import 'dart:convert';
 
-// import 'dart:js' as js;
-// import 'dart:js_util' as js_util;
-// import 'package:js/js.dart';
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
 import 'package:calendar_view/calendar_view.dart';
@@ -13,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -130,17 +128,6 @@ class _HomeState extends State<Home> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: Theme.of(context).colorScheme.primary,
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).colorScheme.primary,
-      //   foregroundColor: Colors.white70,
-      // ),
-      // drawer: Drawer(
-      //   width: MediaQuery.of(context).size.width * 0.9,
-      //   child: Container(
-      //     color: Theme.of(context).colorScheme.secondary,
-      //     child: ListView(children: const []),
-      //   ),
-      // ),
       body: Stack(
         children: [
           Column(
@@ -187,75 +174,7 @@ class _HomeState extends State<Home> {
                               const Spacer(),
                               GestureDetector(
                                 onTap: () {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => AlertDialog(
-                                            title: Text(
-                                              "Settings",
-                                              style: GoogleFonts.poppins(
-                                                  color: Theme.of(context)
-                                                      .colorScheme
-                                                      .primary,
-                                                  fontSize: 25,
-                                                  fontWeight: FontWeight.w700),
-                                            ),
-                                            backgroundColor: Colors.white,
-                                            content: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.center,
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                // Include toggle notifcation,
-                                                TextButton(
-                                                  onPressed: () {
-                                                    FirebaseAuth.instance
-                                                        .signOut();
-                                                    Navigator
-                                                        .pushReplacementNamed(
-                                                            context, '/auth');
-                                                  },
-                                                  style: ButtonStyle(
-                                                    backgroundColor:
-                                                        const MaterialStatePropertyAll(
-                                                            Colors.red),
-                                                    fixedSize:
-                                                        MaterialStatePropertyAll(
-                                                            Size(
-                                                                MediaQuery.of(
-                                                                            context)
-                                                                        .size
-                                                                        .width *
-                                                                    0.8,
-                                                                50)),
-                                                    shape: MaterialStatePropertyAll(
-                                                        RoundedRectangleBorder(
-                                                            borderRadius:
-                                                                BorderRadius
-                                                                    .circular(
-                                                                        20))),
-                                                    elevation:
-                                                        const MaterialStatePropertyAll(
-                                                            8),
-                                                    shadowColor:
-                                                        MaterialStatePropertyAll(
-                                                            Theme.of(context)
-                                                                .colorScheme
-                                                                .secondary),
-                                                  ),
-                                                  child: Text(
-                                                    "Logout",
-                                                    style: GoogleFonts.poppins(
-                                                        color: Colors.white,
-                                                        fontSize: 20,
-                                                        fontWeight:
-                                                            FontWeight.bold),
-                                                  ),
-                                                )
-                                              ],
-                                            ),
-                                          ));
+                                  dialogueBox();
                                 },
                                 child: Padding(
                                   padding: const EdgeInsets.all(10.0),
@@ -673,5 +592,254 @@ class _HomeState extends State<Home> {
         ],
       ),
     );
+  }
+
+  dialogueBox() {
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.white,
+              content: Container(
+                height: MediaQuery.of(context).size.height * 0.7,
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Settings  and close
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 8.0, right: 8, top: 5),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Settings",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.black,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 2),
+                            ),
+                            const Spacer(),
+                            GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Icon(Icons.close_outlined,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                      const SizedBox(),
+                      //LW logo and  built and maintained by Livewires
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/icons/lw_dark.png",
+                            width: 150,
+                            // height: MediaQuery.of(context).size.height * 0.3,
+                            // width: MediaQuery.of(context).size.width * 0.3,
+                          ),
+                          //const SizedBox(),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              "Built & maintained by LiveWires_ ",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      //contributors text
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Contributors",
+                              style: GoogleFonts.poppins(
+                                color: Color.fromARGB(255, 69, 147, 48),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2,
+                              ),
+                            ),
+                            Text(
+                              "🧑🏽\t\tRoshan",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 2),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              "👧🏽\t\tPanchami",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 2),
+                            ),
+                          ],
+                        ),
+                      ),
+                      //follow us
+                      const SizedBox(height: 15),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Follow Us",
+                          style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      //icons
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Row(
+                            children: [
+                              InkWell(
+                                  onTap: () async {
+                                    await launchUrl(Uri.parse(
+                                        "https://www.instagram.com/live.wires_/"));
+                                  },
+                                  child: Image.asset(
+                                    'assets/icons/ig.png',
+                                    height: 30,
+                                    width: 30,
+                                  )),
+                              const Spacer(),
+                              InkWell(
+                                  onTap: () async {
+                                    await launchUrl(Uri.parse(
+                                        "https://x.com/LiveWires_2k23?t=SfRtFlFjs0MBaof2ypydhQ&s=09"));
+                                  },
+                                  child: Image.asset(
+                                    'assets/icons/twt.png',
+                                    height: 30,
+                                    width: 30,
+                                  )),
+                              const Spacer(),
+                              InkWell(
+                                  onTap: () async {
+                                    await launchUrl(Uri.parse(
+                                        "https://www.facebook.com/profile.php?id=61550269514833&mibextid=ZbWKwL"));
+                                  },
+                                  child: Image.asset(
+                                    'assets/icons/fb.png',
+                                    height: 30,
+                                    width: 30,
+                                  )),
+                              const Spacer(),
+                              InkWell(
+                                  onTap: () async {
+                                    await launchUrl(Uri.parse(
+                                        "https://github.com/LiveWiresSRM2023"));
+                                  },
+                                  child: Image.asset(
+                                    'assets/icons/github_round.png',
+                                    height: 30,
+                                    width: 30,
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                      const SizedBox(height: 10),
+                      //join team
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                style: OutlinedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 9, 14, 10),
+                                  foregroundColor:
+                                      const Color.fromARGB(255, 9, 14, 10),
+                                  shadowColor: Colors.black,
+                                  elevation: 2,
+                                  fixedSize: Size(
+                                      MediaQuery.of(context).size.width * 0.8,
+                                      40),
+                                ),
+                                child: Text("Join Team",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w800,
+                                        color: Colors.white))),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                style: OutlinedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 9, 14, 10),
+                                  foregroundColor:
+                                      const Color.fromARGB(255, 9, 14, 10),
+                                  shadowColor: Colors.black,
+                                  elevation: 2,
+                                  fixedSize: Size(
+                                      MediaQuery.of(context).size.width * 0.8,
+                                      40),
+                                ),
+                                child: Text("What would you like us to build",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w800,
+                                        color: Colors.white))),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                style: OutlinedButton.styleFrom(
+                                  backgroundColor:
+                                      Color.fromARGB(255, 37, 103, 50),
+                                  foregroundColor:
+                                      Color.fromARGB(255, 37, 103, 50),
+                                  shadowColor: Color.fromARGB(255, 37, 103, 50),
+                                  elevation: 2,
+                                  fixedSize: Size(
+                                      MediaQuery.of(context).size.width * 0.8,
+                                      40),
+                                ),
+                                child: Text("Logout ?",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w800,
+                                        color: Colors.white))),
+                          ),
+                        ],
+                      ),
+                      //what would u like us to build
+                      //logout?
+                    ]),
+              ),
+            ));
   }
 }
